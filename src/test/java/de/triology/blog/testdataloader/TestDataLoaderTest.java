9f8af52f521a6f9a3eaac86eeb78bbd1481ebf66
Daniel Behrwind <daniel.behrwind@triology.de>
2017-01-26 11:03:52 +0100

    removed @Ignore

--- src/test/java/de/triology/blog/testdataloader/TestDataLoaderTest.java
+++ src/test/java/de/triology/blog/testdataloader/TestDataLoaderTest.java
@@ -42,7 +42,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@Ignore
 public class TestDataLoaderTest {
 
     private TestDataLoader testDataLoader;
